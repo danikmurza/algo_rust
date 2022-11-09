aee2fb599754737c8d8199d8dd61d46da30cb3d8
Daniyar <daniyarmurza@gmail.com>
2022-11-09 15:55:35 -0600

    start

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-
-
 fn main() {
-    let func = algo(348597);
+    let func = count_positives_sum_negatives(vec![]);
 
     println!("{:?}", func);
 }
+fn count_positives_sum_negatives(input: Vec<i32>) -> Vec<i32> {
+    if input.len() == 0 {
+       return  input
+    }
+   let pos =  *input.iter().max().unwrap();
+    let neg =  input.into_iter().filter(|&a| a < 0).reduce(|x, y| x + y).unwrap();
 
+    vec![pos, neg]
 
-fn algo(num: u64) ->Vec<u8> {
-    let mut vec: Vec<u8> = Vec::new();
-    let  split: Vec<u8> = num
-        .to_string()
-        .split("")
-        .filter(|a| a != &"")
-        .map(|b| b.parse::<u8>().unwrap())
-        .collect();
-    for &i in split.iter().rev() {
-        vec.push(i as u8);
-       }
-vec
 }
