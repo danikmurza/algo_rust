90e9763a7f89905e04fa25b9dd1af2e0ad9d9823
Daniyar <daniyarmurza@gmail.com>
2022-11-09 14:41:02 -0600

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let func = algo(348597);
+
+    println!("{:?}", func);
+}
+
+
+fn algo(num: u64) ->Vec<u8> {
+    let mut vec: Vec<u8> = Vec::new();
+    let  split: Vec<u8> = num
+        .to_string()
+        .split("")
+        .filter(|a| a != &"")
+        .map(|b| b.parse::<u8>().unwrap())
+        .collect();
+    for &i in split.iter().rev() {
+        vec.push(i as u8);
+       }
+vec
 }
